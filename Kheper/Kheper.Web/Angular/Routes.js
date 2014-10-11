4dcecb74c293838ccab4db8624e74704b0588f93
Aliaksei Harshkalep <aliaksei_harshkalep@epam.com>
2014-10-11 15:21:45 +0300

    Init work with routes

---
+++ Kheper/Kheper.Web/Angular/Routes.js
@@ -0,0 +1,42 @@
+﻿'use strict';
+
+angular.module('app', ['ui.router'])
+    .config([
+        '$stateProvider', function($stateProvider) {
+            var home = {
+                    name: 'home',
+                    url: '/',
+                    templateUrl: 'Angular/Layout.html'
+                },
+                retrospective = {
+                    name: 'retrospective',
+                    url: '/retrospective',
+                    parent: home,
+                    templateUrl: 'Angular/Retrospective/Layout.html'
+                },
+                planningPocker = {
+                    name: 'planningPocker',
+                    url: '/planningPocker',
+                    parent: home,
+                    templateUrl: 'Angular/PlanningPocker/Layout.html'
+                };
+
+            $stateProvider.state(home);
+            $stateProvider.state(retrospective);
+            $stateProvider.state(planningPocker);
+        }
+    ])
+    .run([
+        '$state', function($state) {
+            $state.transitionTo('home');
+        }
+    ])
+    .controller('SidebarCtrl', function($scope, $state) {
+
+        $scope.content = ['red', 'green', 'blue'];
+
+        $scope.setPage = function(page) {
+            $state.transitionTo(page);
+        };
+    });
+
