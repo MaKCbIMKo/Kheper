e96ffc318e02217895375cf2ea59f770de43b272
Uladzimir Kavaliuk <uladzimir_kavaliuk@epam.com>
2014-10-11 13:23:08 +0300

    Renaming controllers and hubs

---
+++ Kheper/Kheper.Core/Model/PlanningRoom.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kheper.Core.Model
+{
+	public class PlanningRoom
+	{
+		public Guid Id { get; set; }
+
+		public List<VotingSession> Sessions;
+
+		public List<string> Users;
+	}
+}
--- Kheper/Kheper.Web/Hubs/PlanningRoomHub.cs
+++ Kheper/Kheper.Web/Hubs/PlanningRoomHub.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 namespace Kheper.Web.Hubs
 {
 	[HubName("PokerRoomHub")]
-	public class PokerRoomHub : Hub
+	public class PlanningRoomHub : Hub
 	{
 		public override Task OnConnected()
 		{
